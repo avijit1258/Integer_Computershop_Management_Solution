038e82a52a408a67cebc509f0caeb3042d1dd4fc
avijit1258 <avijitbhatt1258@gmail.com>
2017-03-04 03:25:10 +0600

    purchase product sell migration, model, controller created and voucher migration created

---
+++ app/Http/Controllers/SellController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SellController extends Controller
+{
+    public function index(Request $request)
+    {
+    	$products = Product::all();
+        
+        return view('sell.index', compact('products'));
+    }
+
+    /**
+     * Create a new task.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'date' => 'required',
+            'unit_sell_price' => 'required',
+            'total_sell_price' => 'required',
+            'quantity' => 'required',
+            'product_id' => 'required',
+        ]);
+
+        $sell = new Sell;
+
+        $sell->date = $request->date;
+        $sell->quantity = $request->quantity;
+        $sell->unit_sell_price = $request->unit_sell_price;
+        $sell->total_sell_price = $request->total_sell_price;
+        $sell->product_id = $request->product_id;
+
+        $sell->save();
+
+        return redirect('/sells');
+    }
+
+    /**
+     * Destroy the given task.
+     *
+     * @param  Request  $request
+     * @param  Task  $task
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //$this->authorize('destroy', $task);
+
+        //$task->delete();
+
+        Sell::destroy($id);
+
+        return redirect('/sells');
+    }
+}
---
+++ database/migrations/2017_03_03_201333_create_sell_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSellTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sells', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('quantity');
+            $table->integer('product_id');
+            $table->double('unit_sell_price');
+            $table->double('total_sell_price');
+            $table->date('date');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sells');
+    }
+}
